be8dfda643bc849d2603c11ba4bb0ec20df46feb
RafayMughal624 <pressfoam48@gmail.com>
2025-01-06 21:32:36 +0500

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+require('dotenv').config()
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
-
+const PORT = process.env.PORT || 3000
 
 
 
@@ -17,6 +18,6 @@ app.use('/person',usersRoutes)
 const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/menu',menuItemRoutes)
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('server file in running in port 3000')
 })
