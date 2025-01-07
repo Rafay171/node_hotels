287962f4442d7b6462a857d61574a73be071101f
RafayMughal624 <pressfoam48@gmail.com>
2025-01-07 21:34:08 +0500

    Third version in Node Js

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use('/person',usersRoutes)
 const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/menu',menuItemRoutes)
 
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+
+app.get('/', logRequest ,function(req,res){
+    res.send('Welcome to our hotel')
+})
+
 app.listen(PORT,()=>{
     console.log('server file in running in port 3000')
 })
