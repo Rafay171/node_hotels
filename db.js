39c2f7d5cded68aebeda1911818968d75e58a97b
RafayMughal624 <pressfoam48@gmail.com>
2025-01-07 21:35:49 +0500

    Fourth version in Node Js

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 const mongoURL = process.env.MONGODB_URL
 
 mongoose.connect(mongoURL,{
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
 const db = mongoose.connection;
